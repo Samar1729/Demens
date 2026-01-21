52ccfb94f520df69b019539c312bc33e9e53556f
Samar <meet.suraj1994@gmail.com>
2026-01-21 20:09:08 +0530

    made the dropdown menu

--- public/main.js
+++ public/main.js
@@ -1,6 +1,28 @@
-const loginBtn = document.getElementById('loginBtn');
-const userProfile = document.getElementById('userProfile');
-const profileImg = document.getElementById('profileImg');
-const dropdownMenu = document.getElementById('dropdownMenu');
-const logoutBtn = document.getElementById('logoutBtn');
+document.addEventListener('DOMContentLoaded', ()=>{
+    const loginBtn = document.getElementById('loginBtn');
+    const userProfile = document.getElementById('userProfile');
+    const profileImg = document.getElementById('profileImg');
+    const dropdownMenu = document.getElementById('dropdownMenu');
+    const logoutBtn = document.getElementById('logoutBtn');
 
+    const isloggedin = localStorage.getItem('loggedInUserId');
+
+    if (isloggedin){
+    loginBtn.classList.add('hidden');
+    userProfile.classList.remove('hidden')
+    }
+    else {
+    loginBtn.classList.remove('hidden');
+    userProfile.classList.add('hidden');
+    }
+
+    profileImg.addEventListener('click', (e)=> {
+    e.stopPropagation()
+    dropdownMenu.classList.toggle('hidden')
+    })
+
+    logoutBtn.addEventListener('click', ()=>{
+    localStorage.removeItem('loggedInUserId')
+    window.location.reload()
+    })
+})
